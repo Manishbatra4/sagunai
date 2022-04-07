995d8bc101c5987dc2c2170d63068b4d521ef886
Manish Batra <manishbatra004@gmail.com>
2022-04-07 22:22:30 +0530

    v1.0.4

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sagunai
 
 Please re name env.js to .env
+
+Frontend Demo link: https://sagunai.vercel.app/
+
+Backend Demo link: https://as-mb.herokuapp.com/
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
+  //baseURL: "http://localhost:5000/api/",
   baseURL: "https://as-mb.herokuapp.com/api/",
 });
 //  api.defaults.withCredentials = true;
